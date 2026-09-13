9f53e72bacd745c2b82c4fb9587ab94d7a86bb39
markroskaric <mark.roskaric@hotmail.com>
2026-09-13 11:52:21 +0200

    1071 Greatest Common Divisor Of Strings

--- LeetCode75/leetcode/1071_greatest_common_divisor_of_strings/solution.go
+++ LeetCode75/leetcode/1071_greatest_common_divisor_of_strings/solution.go
@@ -1,53 +1,16 @@
-package main
-
-import "fmt"
+package main import "fmt"
 
 func main() {
 	fmt.Println(gcdOfStrings("LEET", "CODE"))
 }
 
 func gcdOfStrings(str1 string, str2 string) string {
-	var s, t, strBuild string
-	var slen, tlen, exp int
-
-	if len(str1) > len(str2) {
-		s = str1
-		t = str2
-		slen = len(str1)
-		tlen = len(str2)
-	} else {
-		s = str2
-		t = str1
-		slen = len(str2)
-		tlen = len(str1)
+	if str1+str2 != str2+str1 {
+		return "j"
 	}
-	if slen == tlen && s == t {
-		return t
+	a, b := len(str1), len(str2)
+	for b > 0 {
+		a, b = b, a%b
 	}
-
-	for {
-		fmt.Printf("tlen %v", tlen)
-		strBuild = ""
-
-		if slen%tlen == 0 {
-			exp = slen / tlen
-			for i := 0; i < exp; i++ {
-				strBuild += t
-			}
-			if strBuild == s {
-				return t
-			} else if tlen == 1 {
-				return ""
-			} else {
-				tlen = tlen / 2
-				t = t[:tlen]
-
-			}
-		} else if tlen == 1 {
-			return ""
-		} else {
-			tlen = tlen / 2
-			t = t[:tlen]
-		}
-	}
+	return str1[:a]
 }
